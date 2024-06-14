9202e4807e36ae4bde6d750a555c0101a74e1187
Mattuser <matheus.stsousa@gmail.com>
2024-06-14 12:49:59 -0300

    Added Expanse and Installment Entities

---
+++ ExpansesApp.Core/Contexts/ExpanseContext/Entities/Expanse.cs
@@ -0,0 +1,16 @@
+﻿using ExpansesApp.Core.Contexts.SharedContext.Entities;
+
+namespace ExpansesApp.Core.Contexts.ExpanseContext.Entities;
+public class Expanse : Entity
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public Installment? Installment { get; set; }
+    public bool IsPaid { get; set; } //Campo processado
+    public bool isInstallment { get; set; } = false;
+    public int TotalInstallments { get; set; }
+    public int RemainingInstallments { get; set; }
+
+
+}
---
+++ ExpansesApp.Core/Contexts/ExpanseContext/Entities/Installment.cs
@@ -0,0 +1,6 @@
+﻿using ExpansesApp.Core.Contexts.SharedContext.Entities;
+
+namespace ExpansesApp.Core.Contexts.ExpanseContext.Entities;
+public class Installment : Entity
+{
+}
