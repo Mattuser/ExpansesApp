03fbcad5b046251bf33e1ba9b8b0abdcbd2bbf5c
Mattuser <matheus.stsousa@gmail.com>
2024-06-14 15:23:28 -0300

    Added entity Category

---
+++ ExpansesApp.Core/Contexts/ExpanseContext/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿using ExpansesApp.Core.Contexts.SharedContext.Entities;
+using System.Xml.Linq;
+
+namespace ExpansesApp.Core.Contexts.ExpanseContext.Entities;
+public class Category : Entity
+{
+    public Category()
+    {
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public Category(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new Exception("Nome não pode ser nulo ou vázio");
+
+        Name = name;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public string Name { get; set; } = string.Empty;
+    public List<Expanse> Expanses { get; set; } = new();
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+}
