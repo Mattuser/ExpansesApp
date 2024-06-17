6c8c53ae962c0f83b1b6e5d37dd90691e216dd48
Mattuser <matheus.stsousa@gmail.com>
2024-06-17 17:34:21 -0300

    Added identation bellow Requires on the Ensure method to the CreateCategorySpecification

--- ExpansesApp.Core/Contexts/Specifications/CreateCategorySpecification.cs
+++ ExpansesApp.Core/Contexts/Specifications/CreateCategorySpecification.cs
@@ -8,6 +8,6 @@ public static class CreateCategorySpecification
     public static Contract<Notification> Ensure(CreateCategoryRequest request)
         => new Contract<Notification>()
             .Requires()
-            .IsLowerThan(request.Name.Length, 40, "Name", "Nome deve conter menos que 40 caracteres")
-            .IsLowerThan(request.Name.Length, 3, "Name", "Nome deve conter mais de 3 caracteres");
+                .IsGreaterThan(request.Name.Length, 3, "Name", "Nome deve conter menos que 40 caracteres")
+                .IsLowerThan(request.Name.Length, 40, "Name", "Nome deve conter mais de 3 caracteres");
 }
