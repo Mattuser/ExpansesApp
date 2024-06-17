247c2ab5ce14023ebc96324cd401b011e6702f73
Mattuser <matheus.stsousa@gmail.com>
2024-06-17 16:49:41 -0300

    Added Action Create to the Category Controller

---
+++ ExpansesApp.Api/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using ExpansesApp.Core.Contexts.ExpanseContext.Contracts.UseCases;
+using ExpansesApp.Core.Contexts.ExpanseContext.UseCases.Create;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpansesApp.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    private readonly ICreateCategoryUseCase _createCategoryUseCase;
+    public CategoryController(ICreateCategoryUseCase createCategoryUseCase)
+    {
+        _createCategoryUseCase = createCategoryUseCase;
+    }
+    public async Task<IResult> Create(CreateCategoryRequest request)
+    {
+        var result = await _createCategoryUseCase.Execute(request, new CancellationToken());
+        return result.IsSuccess
+            ? Results.Created("/category", result)
+            : Results.Json(result, statusCode: result.Status);
+    }
+}
---
+++ ExpansesApp.Core/Contexts/Specifications/CreateCategorySpecification.cs
@@ -0,0 +1,13 @@
+﻿using ExpansesApp.Core.Contexts.ExpanseContext.UseCases.Create;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace ExpansesApp.Core.Contexts.Specification;
+public static class CreateCategorySpecification
+{
+    public static Contract<Notification> Ensure(CreateCategoryRequest request)
+        => new Contract<Notification>()
+            .Requires()
+            .IsLowerThan(request.Name.Length, 40, "Name", "Nome deve conter menos que 40 caracteres")
+            .IsLowerThan(request.Name.Length, 3, "Name", "Nome deve conter mais de 3 caracteres");
+}
