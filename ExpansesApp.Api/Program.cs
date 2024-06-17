52ee004a78013272c6a14aac1b3639d9becbe1b9
Mattuser <matheus.stsousa@gmail.com>
2024-06-17 16:53:59 -0300

    Added dependency injection to the repository and create category usecase

--- ExpansesApp.Api/Program.cs
+++ ExpansesApp.Api/Program.cs
@@ -1,3 +1,7 @@
+using ExpansesApp.Core.Contexts.ExpanseContext.Contracts.Repositories;
+using ExpansesApp.Core.Contexts.ExpanseContext.Contracts.UseCases;
+using ExpansesApp.Core.Contexts.ExpanseContext.UseCases.Create;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICategoryRepository>();
+builder.Services.AddScoped<ICreateCategoryUseCase, CreateCategoryUseCase>();
+
 
 var app = builder.Build();
 
